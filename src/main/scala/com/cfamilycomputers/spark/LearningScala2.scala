484f327e08400143e122540a50d66abad9dac896
Shalendra Kumar <in.shalendra@gmail.com>
2020-08-23 09:32:40 -0300

    added the new scala program

---
+++ src/main/scala/com/cfamilycomputers/spark/LearningScala2.scala
@@ -0,0 +1,54 @@
+package com.cfamilycomputers.spark
+
+object LearningScala2 {
+  // Flow control
+  def main(args: Array[String]): Unit = {
+
+    // If / else syntax
+  if (1 > 3) println("Impossible!") else println("The world makes sense.")
+  //> The world makes sense.
+
+  if (1 > 3) {
+    println("Impossible!")
+  } else {
+    println("The world makes sense.")
+  }                                               //> The world makes sense.
+
+  // Matching - like switch in other languages:
+  val number = 3                                  //> number  : Int = 3
+  number match {
+    case 1 => println("One")
+    case 2 => println("Two")
+    case 3 => println("Three")
+    case _ => println("Something else")
+  }                                         //> Three
+
+  // For loops
+  for (x <- 1 to 4) {
+    val squared = x * x
+    println(squared)
+  }
+  // While loops
+  var x = 10
+  while (x >= 0) {
+    println(x)
+    x -= 1
+  }
+  x = 0
+  do { println(x); x+=1 } while (x <= 10)
+
+  // Expressions
+  // "Returns" the final value in a block automatically
+
+  {val x = 10; x + 20}                           //> res0: Int = 30
+   println("below exercise")
+  println({val x = 10; x + 20})            //> 30
+
+  // EXERCISE
+  // Write some code that prints out the first 10 values of the Fibonacci sequence.
+  // This is the sequence where every number is the sum of the two numbers before it.
+  // So, the result should be 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+
+  }
+}
