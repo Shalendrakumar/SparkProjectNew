5f12a8f4256c1ff15bc18fd012849f4697d380cf
Shalendra Kumar <in.shalendra@gmail.com>
2020-08-22 22:49:02 -0300

    add 10,11

--- src/main/scala/com/cfamilycomputers/spark/MyFirstScala.scala
+++ src/main/scala/com/cfamilycomputers/spark/MyFirstScala.scala
@@ -13,7 +13,7 @@ object MyFirstScala {
 
     //Create RDD
 
-    val rdd1 = sc.makeRDD(Array(1, 2, 3, 4, 5, 6, 7, 8))
+    val rdd1 = sc.makeRDD(Array(1, 2, 3, 4, 5, 6, 7, 8,10,11))
     rdd1.collect().foreach(println)
   }
 }
