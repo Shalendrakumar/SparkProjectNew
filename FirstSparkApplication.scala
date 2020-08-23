2afc0006e2d797e037da88deb9e4b1bca2089505
Shalendra Kumar <in.shalendra@gmail.com>
2020-08-22 22:46:29 -0300

    Initial commit

---
+++ FirstSparkApplication.scala
@@ -0,0 +1,19 @@
+package com.cfamilycomputers.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object FirstSparkApplication {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("First Application")
+
+    val sc = new SparkContext(conf)
+
+    //Create RDD
+
+    val rdd1 = sc.makeRDD(Array(1, 2, 3, 4, 5, 6, 7, 8))
+    rdd1.collect().foreach(println)
+  }
+}
